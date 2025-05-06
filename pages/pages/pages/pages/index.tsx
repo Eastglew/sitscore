b9ba7ece4c0f519f18472754c50f99b93f0803c6
Eastglew <kurelax@gmail.com>
2025-05-06 08:51:14 -0700

    Create index.tsx

---
+++ pages/pages/pages/pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: '2rem', fontFamily: 'Arial' }}>
+      <h1>Welcome to SitScore 👋</h1>
+      <p>This is your homepage. It's working!</p>
+    </main>
+  );
+}

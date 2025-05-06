b5eb8afbc11d5bd86f4515fa0872bbcf86d7fd00
Eastglew <kurelax@gmail.com>
2025-05-06 12:17:20 -0700

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,8 +1,1 @@
-export default function Home() {
-  return (
-    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h1>Welcome to SitScore</h1>
-      <p>This is your new trust verification platform for pet sitters.</p>
-    </main>
-  );
-}
+
